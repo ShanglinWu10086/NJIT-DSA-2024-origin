8ee837e3b7bf1f48506d8d98f926ef2a6e84e36a
ShanglinWu10086 <2462555848@qq.com>
2024-04-11 08:13:47 +0800

    SUBMITTED

--- 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
+++ 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
@@ -5,86 +5,69 @@ public class Algorithms {
         // Empty
     }
 
-    public static int binarySearch(int target, Integer[] array, int fromIndex, int toIndex) {
-        int left = fromIndex; 
-        int right = toIndex;
-        while (left <= right) {
-            int mid = (right + left) / 2;
-            if(array[mid] == target){
-                return mid;
-            }else if(array[mid] < target){
-                left = mid + 1;
-            }else{
-                right = mid - 1;
+    public static <T extends Comparable<T>> void sort(T[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].compareTo(array[j]) > 0) {
+                    T temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
             }
-            
         }
-
-        return -1;
     }
 
-    public static int binarySearch(String value, String[] array, int fromIndex, int toIndex) {
+    public static <T> void reverse(T[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            T temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+    }
+
+    public static <T extends Comparable<T>> int binarySearch(T target, T[] array, int fromIndex, int toIndex) {
         int left = fromIndex;
         int right = toIndex;
-
         while (left <= right) {
             int mid = (right + left) / 2;
-            int cmp = value.compareTo(array[mid]);
-
+            int cmp = array[mid].compareTo(target);
             if (cmp == 0) {
-                return mid; 
+                return mid;
             } else if (cmp < 0) {
-                right = mid + 1; 
+                left = mid + 1;
             } else {
-                left = mid - 1; 
+                right = mid - 1;
             }
         }
-
         return -1;
     }
-    public static void sort(Integer[] array) {
-        int n = array.length;
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
+
+    public static <T extends Comparable<T>> void fastSort(T[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static <T extends Comparable<T>> void quickSort(T[] array, int start, int end) {
+        if (start < end) {
+            int pivotIndex = partition(array, start, end);
+            quickSort(array, start, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, end);
         }
     }
-    }
 
-
-    public static void sort(String[] array) {
-       quickSort(array, 0, array.length - 1);
-    }  
-
-   public static void quickSort(String[] array, int start, int end){
-    if(start < end){
-       int pivotIndex = partition(array, start, end);
-       quickSort(array, start, pivotIndex - 1);
-       quickSort(array, pivotIndex + 1, end);
-    }
-
-   }
-    private static int partition(String[] array, int start, int end){
-        String pivot = array[end];
+    private static <T extends Comparable<T>> int partition(T[] array, int start, int end) {
+        T pivot = array[end];
         int i = start - 1;
-        for(int j = start; j < end; j++){
-            if(array[j].compareTo(pivot) < 0){
+        for (int j = start; j < end; j++) {
+            if (array[j].compareTo(pivot) < 0) {
                 i++;
-                String tmp = array[i];
+                T tmp = array[i];
                 array[i] = array[j];
                 array[j] = tmp;
             }
-        } 
-        String tmp = array[i + 1];
+        }
+        T tmp = array[i + 1];
         array[i + 1] = array[end];
         array[end] = tmp;
         return i + 1;
     }
-    
-
-
 }
